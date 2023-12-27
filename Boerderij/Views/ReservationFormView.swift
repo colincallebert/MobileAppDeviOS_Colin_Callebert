2a2a5e1a43dd9aab3ce752e7af1e360eeb150cb5
colincallebert <72252025+colincallebert@users.noreply.github.com>
2023-12-27 19:35:24 +0100

    adding validation

--- Boerderij/Views/ReservationFormView.swift
+++ Boerderij/Views/ReservationFormView.swift
@@ -12,6 +12,9 @@ struct ReservationFormView: View {
     @State private var alertMessage = ""
     @State private var alertTitle = ""
     
+    @State private var isEmailValid = true
+    @State private var isEmailEntered = false
+    
     @Environment(\.presentationMode) var presentationMode
     
     @StateObject var contentController = ContentController()
@@ -22,24 +25,41 @@ struct ReservationFormView: View {
                 Text("Reservatie voor \(activity.title)")
                     .font(.title)
                     .bold()
-                Text("Voornaam:")
+                Text("Voornaam")
                     .font(.headline)
                 TextField("Voornaam", text: $firstName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Achternaam:")
+                Text("Achternaam")
                     .font(.headline)
                 TextField("Achternaam", text: $lastName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 
-                Text("Email:")
-                    .font(.headline)
+                HStack {
+                    Text("Email")
+                    Text("*")
+                        .foregroundColor(.red)
+                }
+                .font(.headline)
                 TextField("Email", text: $userEmail)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .onChange(of: userEmail) {_, newEmail in
+                        isEmailValid = isValidEmail(email: newEmail)
+                        isEmailEntered = !newEmail.isEmpty
+                    }
+                
+                if !isEmailEntered {
+                    Text("Geef je emailadres in")
+                        .foregroundColor(.red)
+                } else if !isEmailValid {
+                    Text("Geen een geldig emailadres in")
+                        .foregroundColor(.red)
+                }
                 
                 Stepper("Aantal Personen: \(numberOfPeople)", value: $numberOfPeople, in: 1...activity.maxregistrations)
                     .font(.headline)
-               
-                    Button(action: {
+                
+                Button(action: {
+                    if (isEmailValid && isEmailEntered) {
                         Task {
                             do {
                                 try await contentController.registreren(activityId: activity.id, amount: numberOfPeople)
@@ -52,15 +72,16 @@ struct ReservationFormView: View {
                                 showAlert = true
                             }
                         }
-                    }) {
-                        Text("Reserveren")
-                            .padding()
-                            .background(Color(#colorLiteral(red: 0.15, green: 0.45, blue: 0.2, alpha: 1.0)))
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
                     }
-                    .padding()
-                   
+                }) {
+                    Text("Reserveren")
+                        .padding()
+                        .background(Color(#colorLiteral(red: 0.15, green: 0.45, blue: 0.2, alpha: 1.0)))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding()
+                
             }
             .padding()
             .alert(isPresented: $showAlert) {
@@ -72,7 +93,13 @@ struct ReservationFormView: View {
                     })
                 )
             }
+            
         }
+        
     }
-    
+    func isValidEmail(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
 }
